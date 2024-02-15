90edbef575c1c4f72adaa84a4a195bcf8ab465a4
Abhinay Goswami <abhinay.21GCEBAIDS059@galgotiacollege.edu>
2024-02-15 21:36:09 +0530

    Create POTD-2(15Feb.2024).cpp

---
+++ POTD-2(15Feb.2024).cpp
@@ -0,0 +1,26 @@
+//Find Polygon With the Largest Perimeter
+
+//Approach-1:Prefix sum and sorting
+class Solution {
+public:
+    long long largestPerimeter(vector<int>& nums) {
+        sort(nums.begin(),nums.end());//time :nlogn
+        vector<long long>prefixsum(nums.size());//space:n
+        prefixsum[0]=nums[0];
+        for(int i=1;i<nums.size();i++){
+            prefixsum[i]=prefixsum[i-1]+nums[i];
+        }
+        int side=nums.size();
+        for(int i=nums.size()-1;i>=2;i--){
+            if(nums[i]>=(prefixsum[i]-nums[i]))
+            {
+                side--;
+            }
+            else{
+                return prefixsum[i];
+            }
+        }
+        return -1;
+
+    }
+};
