8495b32bda7708b325285a8b82273236d08b025b
Abhinay Goswami <abhinay.21GCEBAIDS059@galgotiacollege.edu>
2024-02-15 23:23:03 +0530

    Update POTD-1(14feb.2024).cpp

--- POTD-1(14feb.2024).cpp
+++ POTD-1(14feb.2024).cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& nums) {
+        //    Appproach-1
         int n=nums.size();
         vector<int>positive;
         vector<int>negative;
@@ -19,6 +20,23 @@ public:
             result.push_back(negative[i]);
         }
         return result;
+
+        //Approach-2
+        int n = nums.size();
+        vector<int> ans(n, 0);
+        int posIndex = 0, negIndex=1;
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] > 0) {
+                ans[posIndex] = nums[i];
+                posIndex+=2;
+            } else {
+                ans[negIndex] = nums[i];
+                negIndex += 2;
+            }
+        }
+
+        return ans;
       
     }
 };
