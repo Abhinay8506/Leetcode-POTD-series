14ed912918a3b9ce204b8f9da69c4e3d08942f74
Abhinay Goswami <abhinay.21GCEBAIDS059@galgotiacollege.edu>
2024-02-22 23:24:11 +0530

    Update POTD-9(22 feb.2024).cpp

--- POTD-9(22 feb.2024).cpp
+++ POTD-9(22 feb.2024).cpp
@@ -2,14 +2,14 @@ class Solution {
 public:
 
     int findJudge(int n, vector<vector<int>>& trust) {
-        vector<int> in(n + 1); 
-        vector<int> out(n + 1);
+        vector<int> independent(n + 1); 
+        vector<int> dependent(n + 1);
 		for (auto a : trust) {
-			++out[a[0]];
-			++in[a[1]];
+			++dependent[a[0]];
+			++independent[a[1]];
 		}
 		for (int i = 1; i <= n; i++) {
-			if (in[i] == n - 1 && out[i] == 0) return i;
+			if (independent[i] == n - 1 && dependent[i] == 0) return i;
 		}
 		return -1; 
     }
