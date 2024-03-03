da792b6ae17886531ce194377cf84b4bb845144c
Abhinay Goswami <abhinay.21GCEBAIDS059@galgotiacollege.edu>
2024-03-03 22:34:26 +0530

    Update POTD-18(Squares of sorted array:02march 2024).cpp

--- POTD-18(Squares of sorted array:02march 2024).cpp
+++ POTD-18(Squares of sorted array:02march 2024).cpp
@@ -1,3 +1,17 @@
+//APPROACH-1
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+        vector<int>v(nums.size());
+        for(int i=0;i<nums.size();i++){
+            v[i]=nums[i]*nums[i];
+        }
+        sort(v.begin(),v.end());
+        return v;
+    }
+};
+//APPROACH-2
+
 class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
